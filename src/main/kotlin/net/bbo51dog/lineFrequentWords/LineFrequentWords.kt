fb1b7b85b3e6a12cf82e472a934a35411f465723
bbo51dog <bbo51dog@gmail.com>
2020-04-04 21:54:33 +0900

    import file

--- src/main/kotlin/net/bbo51dog/lineFrequentWords/LineFrequentWords.kt
+++ src/main/kotlin/net/bbo51dog/lineFrequentWords/LineFrequentWords.kt
@@ -12,6 +12,15 @@ class LineFrequentWords {
                 println("Please enter the LINE log file path as the first argument")
                 exitProcess(1)
             }
+            val logFile = File(fileName)
+            val messageRegex = Regex("""[0-1][0-9]|2[0-3]:[0-5][0-9]\t.*\t.*""")
+            val replaceRegex = Regex("""[0-1][0-9]|2[0-3]:[0-5][0-9]\t.*\t""")
+            logFile.forEachLine {
+                if (!messageRegex.matches(it)) {
+                    return@forEachLine
+                }
+                var message = it.replace(replaceRegex, "")
+            }
         }
     }
 }
