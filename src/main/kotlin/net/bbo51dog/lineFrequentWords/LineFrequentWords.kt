bd0414f41a246617afca1375ca001eafb350b1ab
bbo51dog <bbo51dog@gmail.com>
2020-04-05 12:21:53 +0900

    part of speech

--- src/main/kotlin/net/bbo51dog/lineFrequentWords/LineFrequentWords.kt
+++ src/main/kotlin/net/bbo51dog/lineFrequentWords/LineFrequentWords.kt
@@ -16,6 +16,12 @@ class LineFrequentWords {
             val logFile = File(fileName)
             val messageRegex = Regex("""([0-1][0-9]|2[0-3]):[0-5][0-9]\t.+\t.+""")
             val replaceRegex = Regex("""([0-1][0-9]|2[0-3]):[0-5][0-9]\t.+\t""")
+            val ignorePart = setOf(
+                "助詞",
+                "助動詞",
+                "記号",
+                "フィラー"
+            )
             val tokenizer = Tokenizer()
             val words = mutableMapOf<String, Int>()
             var messageCount = 0
@@ -30,15 +36,17 @@ class LineFrequentWords {
                 }
                 var tokens = tokenizer.tokenize(message)
                 tokens.forEach {token ->
-                    words[token.surface] = if (words.containsKey(token.surface)) {
-                        words[token.surface]!!.toInt() + 1
+                    if (ignorePart.contains(token.partOfSpeechLevel1)) return@forEach
+                    if(token.surface.length <= 2) return@forEach
+                    words[token.surface + token.partOfSpeechLevel1] = if (words.containsKey(token.surface + token.partOfSpeechLevel1)) {
+                        words[token.surface + token.partOfSpeechLevel1]!!.toInt() + 1
                     } else {
                         1
                     }
                 }
             }
             var sorted: MutableMap<String, Int> = words.toList().sortedByDescending{ it.second }.toMap().toMutableMap()
-            for (i in 1..10) {
+            for (i in 1..20) {
                 if (sorted.isEmpty()) break
                 var word = sorted.keys.first()
                 var count = sorted[word]
